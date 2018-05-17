2830895ce26319d71918a17735845e88ca7b52f7
Johan Rosell <rossleur@protonmail.com>
2018-05-17 14:27:13 +0200

    build with ghc822

--- default.nix
+++ default.nix
@@ -1,13 +1,10 @@
-{ mkDerivation, aeson, base, ghc-mod, hindent, hlint, QuickCheck
-, stdenv, time, transformers
+{ mkDerivation, aeson, base, QuickCheck, stdenv, time, transformers
 }:
 mkDerivation {
   pname = "eventsourcing";
   version = "0.0.0.1";
   src = ./.;
-  libraryHaskellDepends = [
-    aeson base ghc-mod hindent hlint time transformers
-  ];
+  libraryHaskellDepends = [ aeson base time transformers ];
   testHaskellDepends = [ aeson base QuickCheck time transformers ];
   homepage = "https://github.com/jrsll/eventsourcing#readme";
   license = stdenv.lib.licenses.bsd3;
--- shell.nix
+++ shell.nix
@@ -1,5 +1,23 @@
-let
-  x = import ./release0.nix { compiler = "ghc802"; withHoogle = true; };
-in
-  x.env
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc822" }:
 
+let
+
+  inherit (nixpkgs) pkgs;
+
+  hpkgs = pkgs.haskell.packages.${compiler}.extend (
+    self: super: {
+      ghc =
+        super.ghc // { withPackages = super.ghc.withHoogle; };
+      ghcWithPackages =
+        self.ghc.withPackages;
+    }
+  ); 
+
+  drv = pkgs.haskell.lib.addBuildTool (
+    hpkgs.callPackage (import ./default.nix) {}
+  ) [ hpkgs.hlint
+      hpkgs.stylish-haskell ];
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
